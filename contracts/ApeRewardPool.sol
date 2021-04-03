c69d5d0c256e8840675025bb19577e426a3abc2a
defifofum <defifofum0@gmail.com>
2021-04-03 16:03:53 -0400

    rename BEP20RewardApe

--- contracts/ApeRewardPool.sol
+++ contracts/ApeRewardPool.sol
@@ -11,12 +11,14 @@ pragma solidity ^0.8.0;
  * GitHub:          https://github.com/ApeSwapFinance
  */
 
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import './FactoryOwnable.sol';
 import './BEP20/IBEP20.sol';
 
-contract BEP20RewardApeV2 is Ownable {
+// TODO: add functionality to accurately calculate stake tokens values 
+contract ApeRewardPool is Initializable, FactoryOwnable {
     using SafeMath for uint256;
     using SafeERC20 for IBEP20;
 
@@ -37,10 +39,12 @@ contract BEP20RewardApeV2 is Ownable {
     // The stake token
     IBEP20 public stakeToken;
     // The reward token
-    IBEP20 public rewardToken;
+    address public rewardToken;
+    // 
+    bool public isBNBRewardPool = false;
 
     // Reward tokens created per block.
-    uint256 public rewardPerBlock;
+    uint256 public rewardPerBlock = 0;
     // Keep track of total rewards assigned
     uint256 internal totalRewards = 0;
     // Total Reward Debt 
@@ -67,17 +71,19 @@ contract BEP20RewardApeV2 is Ownable {
     event EmergencyRewardWithdraw(address indexed user, uint256 amount);
     event EmergencySweepWithdraw(address indexed user, IBEP20 indexed token, uint256 amount);
 
-
-    constructor(
+    // TEST: this cannot be called twice
+    function initialize(
         IBEP20 _stakeToken,
-        IBEP20 _rewardToken,
-        uint256 _rewardPerBlock,
+        address _rewardToken,
         uint256 _startBlock,
         uint256 _bonusEndBlock
-    ) {
+    ) public initializer {
         stakeToken = _stakeToken;
         rewardToken = _rewardToken;
-        rewardPerBlock = _rewardPerBlock;
+        // TEST: that address 0 works
+        if(rewardToken == address(0)) {
+            isBNBRewardPool = true;
+        }
         startBlock = _startBlock;
         bonusEndBlock = _bonusEndBlock;
 
@@ -90,18 +96,8 @@ contract BEP20RewardApeV2 is Ownable {
         }));
 
         totalAllocPoint = 1000;
-
-    }
-
-    // Return reward multiplier over the given _from to _to block.
-    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
-        if (_to <= bonusEndBlock) {
-            return _to.sub(_from);
-        } else if (_from >= bonusEndBlock) {
-            return 0;
-        } else {
-            return bonusEndBlock.sub(_from);
-        }
+        // TEST: that the proper factory is calling this
+        transferFactoryInternal(_msgSender());
     }
 
     // View function to see pending Reward on frontend.
@@ -120,6 +116,8 @@ contract BEP20RewardApeV2 is Ownable {
         // Multiply the user amount of staked tokens multiplied by the 
         return user.amount.mul(accRewardTokenPerShare).div(1e12).sub(user.rewardDebt);
     }
+
+    /* Public Functions */
 
     // Update reward variables of the given pool to be up-to-date.
     function updatePool(uint256 _pid) public {
@@ -146,7 +144,6 @@ contract BEP20RewardApeV2 is Ownable {
             updatePool(pid);
         }
     }
-
 
     /// Deposit staking token into the contract to earn rewards. 
     /// @dev Since this contract needs to be supplied with rewards we are 
@@ -207,10 +204,39 @@ contract BEP20RewardApeV2 is Ownable {
         emit Withdraw(msg.sender, _amount);
     }
 
+    /// @notice Update the reward per block based on the endblock and current rewards in contract
+    /// @dev This function is PUBLIC so that anyone can update the rewards per block. 
+    ///   This may be useful for reflect tokens or other deviations
+    function updateRewardPerBlock() public {
+        uint256 blockDiff = bonusEndBlock.sub(getNextRewardBlock());
+        rewardPerBlock = availableRewards().div(blockDiff);
+        emit UpdateRewardPerBlock(rewardPerBlock);
+    }
+
+    /* Public View Functions */
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from);
+        } else if (_from >= bonusEndBlock) {
+            return 0;
+        } else {
+            return bonusEndBlock.sub(_from);
+        }
+    }
+
     /// Obtain the reward balance of this contract
     /// @return wei balace of conract
     function rewardBalance() public view returns (uint256) {
-        return rewardToken.balanceOf(address(this));
+        if(isBNBRewardPool) {
+            // TEST: return BNB balance of this contract
+            // Return BNB balance
+            return payable(address(this)).balance;
+        } else {
+            // Return BEO20 balance
+            return IBEP20(rewardToken).balanceOf(address(this));
+        }
     }
 
     /// Get the amount of rewards that are left to be paid out
@@ -223,7 +249,7 @@ contract BEP20RewardApeV2 is Ownable {
         return rewardBalance().sub(rewardsLeftToPay());
     }
 
-    function getNextRewardBlock() internal view returns (uint256) {
+    function getNextRewardBlock() public view returns (uint256) {
         uint256 currentBlock = block.number;
         if(startBlock > currentBlock) {
             return startBlock;
@@ -232,21 +258,26 @@ contract BEP20RewardApeV2 is Ownable {
         }
     }
 
-    // Deposit Rewards into contract
-    function depositRewards(uint256 _amount) external {
+    /* External Functions */
+
+    // TEST: test
+    // Deposit BEP20 Rewards into contract
+    function depositBEP20Rewards(uint256 _amount) external {
+        require(!isBNBRewardPool, 'Cannot deposit BEP20 rewards into a BNB reward pool');
         require(_amount > 0, 'Deposit value must be greater than 0.');
-        rewardToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+        IBEP20(rewardToken).safeTransferFrom(address(msg.sender), address(this), _amount);
         updateRewardPerBlock();
         emit DepositRewards(_amount);
     }
 
-    /// @notice Update the reward per block based on the endblock and current rewards in contract
-    /// @dev This function is PUBLIC so that anyone can update the rewards per block. 
-    ///   This may be useful for reflect tokens or other deviations
-    function updateRewardPerBlock() public {
-        uint256 blockDiff = bonusEndBlock.sub(getNextRewardBlock());
-        rewardPerBlock = availableRewards().div(blockDiff);
-        emit UpdateRewardPerBlock(rewardPerBlock);
+    // TEST:
+    // Deposit BNB Rewards into contract
+    function depositBNBRewards() external payable {
+        require(isBNBRewardPool, 'Cannot deposit BNB rewards into a BEP20 reward pool');
+        require(msg.value > 0, 'Message has no BNB value to deposit into contract.');
+        // TEST that this updates the reward per block automatically
+        updateRewardPerBlock();
+        emit DepositRewards(msg.value);
     }
 
     /* Internal Functions */
@@ -254,8 +285,16 @@ contract BEP20RewardApeV2 is Ownable {
     /// @param _to address to send reward token to
     /// @param _amount value of reward token to transfer
     function _safeTransferReward(address _to, uint256 _amount) internal {
-        totalRewardDebt = totalRewardDebt.add(_amount);
-        rewardToken.safeTransfer(_to, _amount);
+        if(isBNBRewardPool) {
+            // TEST: transfer BNB
+            // Transfer BNB to address
+            (bool success, ) = _to.call{gas: 23000, value: _amount}("");
+            require(success, 'TransferHelper: BNB_TRANSFER_FAILED'); 
+        } else {
+            // Transfer BEP20 to address
+            totalRewardDebt = totalRewardDebt.add(_amount);
+            IBEP20(rewardToken).safeTransfer(_to, _amount);
+        }
     }
 
 
@@ -263,7 +302,7 @@ contract BEP20RewardApeV2 is Ownable {
     
     /// @notice Update the block where rewards end
     /// @param  _bonusEndBlock The block when rewards will end
-    function setBonusEndBlock(uint256 _bonusEndBlock) external onlyOwner {
+    function updateBonusEndBlock(uint256 _bonusEndBlock) external onlyFactoryOwner {
         require(_bonusEndBlock > getNextRewardBlock(), 'new bonus end block must be greater than the next reward block');
         bonusEndBlock = _bonusEndBlock;
         updateRewardPerBlock();
@@ -283,7 +322,7 @@ contract BEP20RewardApeV2 is Ownable {
     }
 
     // Withdraw reward. EMERGENCY ONLY.
-    function emergencyRewardWithdraw(uint256 _amount) external onlyOwner {
+    function emergencyRewardWithdraw(uint256 _amount) external onlyFactoryOwner {
         require(_amount <= rewardBalance(), 'not enough rewards');
         // Withdraw rewards
         _safeTransferReward(address(msg.sender), _amount);
@@ -293,11 +332,10 @@ contract BEP20RewardApeV2 is Ownable {
     /// @notice A public function to sweep accidental BEP20 transfers to this contract. 
     ///   Tokens are sent to owner
     /// @param token The address of the BEP20 token to sweep
-    function sweepToken(IBEP20 token) external onlyOwner {
+    function sweepToken(IBEP20 token) external onlyFactoryOwner {
         require(address(token) != address(stakeToken), "can not sweep stake token");
         uint256 balance = token.balanceOf(address(this));
         token.transfer(msg.sender, balance);
         emit EmergencySweepWithdraw(msg.sender, token, balance);
     }
-
 }
