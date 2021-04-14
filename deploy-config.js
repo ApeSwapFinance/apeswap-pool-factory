3713713caa6d8f7b06d2c95ae8b272946fe9d140
defifofum <defifofum0@gmail.com>
2021-04-14 15:16:25 -0400

    refactor: Migrations

--- deploy-config.js
+++ deploy-config.js
@@ -3,6 +3,7 @@ function getNetworkConfig(network, accounts) {
     if(["bsc", "bsc-fork"].includes(network)) {
         console.log(`Deploying with BSC MAINNET config.`)
         return {
+            apePairFactoryOwner: '0x6c905b4108A87499CEd1E0498721F2B831c6Ab13', // ApeSwap General Admin
             apePairFactory: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6', // Ape Factory
             feeToken: '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95', // BANANA
             feeAmount: '10000000000000000000000', // 10,000 
@@ -10,15 +11,16 @@ function getNetworkConfig(network, accounts) {
     } else if (['testnet', 'testnet-fork'].includes(network)) {
         console.log(`Deploying with BSC testnet config.`)
         return {
-            // TODO: Update values
-            apePairFactory: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6', // Ape Factory
-            feeToken: '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95', // BANANA
+            apePairFactoryOwner: '0xE375D169F8f7bC18a544a6e5e546e63AD7511581', // ApeSwap Testnet
+            apePairFactory: '0x152349604d49c2Af10ADeE94b918b051104a143E', // Ape Factory
+            feeToken: '0x4Fb99590cA95fc3255D9fA66a1cA46c43C34b09a', // BANANA
             feeAmount: '0',
         }
     } else if (['development'].includes(network)) {
         console.log(`Deploying with development config.`)
         return {
             // TODO: Update values
+            apePairFactoryOwner: '0x6c905b4108A87499CEd1E0498721F2B831c6Ab13', // ApeSwap General Admin (Trezor 2)
             apePairFactory: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6', // Ape Factory
             feeToken: '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95', // BANANA
             feeAmount: '0', 
--- migrations/2_deploy_ApeRewardPoolFactory.js
+++ migrations/2_deploy_ApeRewardPoolFactory.js
@@ -1,13 +1,17 @@
 const ApeRewardPoolFactory = artifacts.require("ApeRewardPoolFactory");
 const { getNetworkConfig } = require('../deploy-config')
 
+// TODO: Add proxy deployment
 module.exports = async function (deployer, network, accounts) {
   const deployConfig = getNetworkConfig(network);
 
   console.log("Deploying ApePoolFactory with config:");
   console.dir(deployConfig);
 
-  const { apePairFactory, feeToken, feeAmount } = deployConfig;
+  const { apePairFactoryOwner, apePairFactory, feeToken, feeAmount } = deployConfig;
 
   await deployer.deploy(ApeRewardPoolFactory, apePairFactory, feeToken, feeAmount);
+  const apeRewardPoolFactory =  await ApeRewardPoolFactory.at(ApeRewardPoolFactory.address);
+  await apeRewardPoolFactory.transferOwnership(apePairFactoryOwner);
+  
 };
