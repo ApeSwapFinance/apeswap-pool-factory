eeb6b9943d854f7a3ec1e23fe42e628ff496b72b
defifofum <defifofum0@gmail.com>
2022-02-11 14:02:02 -0500

    feat: Add support files for fee script

---
+++ scripts/constants/poolConfig.ts
@@ -0,0 +1,8 @@
+import { PoolCategory } from './types'
+import tokens from './tokens'
+
+// Pull pool config from the following file:
+// https://github.com/ApeSwapFinance/apeswap-frontend/blob/main/src/config/constants/pools.ts
+const pools = [];
+
+export default pools
---
+++ scripts/constants/tokens.ts
@@ -0,0 +1,5 @@
+// Pull token list from the following file:
+// https://raw.githubusercontent.com/ApeSwapFinance/apeswap-frontend/main/src/config/constants/tokens.ts
+const tokens = {}
+
+export default tokens
